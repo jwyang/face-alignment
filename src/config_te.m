bb4b93da941aebcc22d3872d146b4054b8245d81
jwyang <jw2.yang@gmail.com>
2014-11-21 12:09:34 +0800

    modify configure file

--- src/config_te.m
+++ src/config_te.m
@@ -7,17 +7,17 @@ params.augnumber_shift  = 0;   % the number of initial shapes in augmenting trai
 params.augnumber_rotate = 0;   % the number of initial shapes in augmenting training set by rotation
 params.augnumber_scale  = 0;   % the number of initial shapes in augmenting training set by rotation
 params.flipflag         = 0;   % the flag of flipping
-params.ind_usedpts      = 18:68;
+params.ind_usedpts      = 1:68;
 params.augnumber        = (params.augnumber_shift + 1)*(params.augnumber_rotate + 1)*((params.augnumber_scale + 1));
 
 % initialize parameters for training random forest
 params.max_numfeats    = [500 500 500 500 500 500 400 400];
 params.bagging_overlap = 0.4;
-params.max_raio_radius = [0.4 0.3 0.2 0.15 0.1 0.1 0.08 0.08 0.06 0.05];
+params.max_raio_radius = [0.3 0.2 0.2 0.15 0.12 0.10 0.08 0.06 0.06 0.05]; % [0.4 0.4 0.35 0.3 0.2 0.10 0.08 0.06 0.06 0.05];
 params.max_numtrees    = 10;
-params.max_depth       = 5;
+params.max_depth       = 6;
 
 params.max_numthreshs  = 100;
 
 % initialize parameters for boosting gradient regression
-params.max_numstage    = 7;
+params.max_numstage    = 5;
